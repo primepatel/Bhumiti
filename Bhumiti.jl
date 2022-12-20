1e252e09a572026aa6c442cff452b4a52a8629a9
Prime Patel <primespatel@gmail.com>
2022-12-21 01:42:27 +0530

    presentation

---
+++ Bhumiti.jl
@@ -0,0 +1,22 @@
+module Bhumiti
+
+import Base: show
+
+using DynamicPolynomials
+using OrderedCollections
+
+export @hp, @th, Theorem, prove, Coordinate, RittWuMethod, isproved, ndg
+
+include("geometry/primitives.jl")
+include("geometry/constraints.jl")
+include("geometry/point_status.jl")
+
+include("theorem/theorem.jl")
+include("theorem/parse_constraints.jl")
+include("theorem/parse_theorem.jl")
+
+include("rittwu/coordinates.jl")
+include("rittwu/algebra.jl")
+include("rittwu/triangulize.jl")
+include("rittwu/prove.jl")
+end
